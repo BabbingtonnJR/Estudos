1eadb3e1b174bddf77eb0974b02512347e6f45eb
Babbingtonn Luiz Pereira Junior <162913182+BabbingtonnJR@users.noreply.github.com>
2026-09-02 15:09:17 -0300

    Delete projetos/projeto-java/Sistema directory

--- projetos/projeto-java/Sistema/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module br.pucpr.projeto.sistema {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens br.pucpr.projeto.sistema to javafx.fxml;
-    exports br.pucpr.projeto.sistema;
-}
